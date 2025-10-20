5aa64e223ec4b3d0d9d4c3dc51573a95dda6a8fa
Amogh Singh <amogh.singh253@gmail.com>
2025-10-21 00:54:10 +0530

    Adding in tests for gene sim only

--- src/tests/test_sim.py
+++ src/tests/test_sim.py
@@ -2,13 +2,34 @@ from simulator import gpsim
 import numpy as np
 
 
-def test_calc_steady_states():
-    # Dummy test to check if initialization works and simulator runs
-    sim = gpsim.simulator(
-        config_file="configs/sample_data/sample_network_2cell.yaml", n_cells=2
-    )
-    gene_conc, prot_conc = sim.run_sim(10)
-    gene_conc, prot_conc = np.array(gene_conc), np.array(prot_conc)
-    print(gene_conc.shape, prot_conc.shape)
-    assert gene_conc.shape == (10, 4, 2)
-    assert prot_conc.shape == (10, 4, 2)
+class TestFile:
+    config_files = [
+        "configs/sample_data/sample_network_2cell.yaml",
+        "configs/sample_data/sample_network_1cell.yaml",
+    ]
+    cell_no = [2, 1]
+
+    def test_calc_steady_states(self):
+        # Dummy test to check if initialization works and simulator runs
+        for i in range(len(self.config_files)):
+            sim = gpsim.simulator(
+                config_file=self.config_files[i], n_cells=self.cell_no[i]
+            )
+            gene_conc, prot_conc = sim.run_sim(10)
+            gene_conc, prot_conc = np.array(gene_conc), np.array(prot_conc)
+            print(gene_conc.shape, prot_conc.shape)
+            assert gene_conc.shape == (10, 4, self.cell_no[i])
+            assert prot_conc.shape == (10, 4, self.cell_no[i])
+
+    def gene_only_test(self):
+        for i in range(len(self.config_files)):
+            sim = gpsim.simulator(
+                config_file=self.config_files[i],
+                n_cells=self.cell_no[i],
+                protein_sim=False,
+            )
+            gene_conc, prot_conc = sim.run_sim(10)
+            gene_conc, prot_conc = np.array(gene_conc), np.array(prot_conc)
+            print(gene_conc.shape, prot_conc.shape)
+            assert gene_conc.shape == (10, 4, self.cell_no[i])
+            assert prot_conc.shape == (10, 4, self.cell_no[i])
